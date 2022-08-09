e5cead7f283fdef3df707e62f5213479aa6b73c7
PranaliSatape <110046287+PranaliSatape@users.noreply.github.com>
2022-08-09 19:35:10 +0530

    Add files via upload

---
+++ day_5/datatypes.py
@@ -0,0 +1,29 @@
+from types import NoneType
+
+
+Name= "Pranali"
+string(Name)
+x = renge(10)
+print (type(x))
+dict = {"name" : "shiv", "age" : 25}
+print(type(dict))
+fruitSet= {"apple", "pear", "grapes"}
+print(type(fruitSet))
+fruitFrozenSet= ({"apple", "pear", "grapes"})
+print(type(fruitFrozenSet))
+boolValue = True
+print(type(boolValue)
+#bytearray = bytearray(10)
+#print(type(bytearray))
+#NoneType= None
+#print(type(NoneType))
+#tuple = ("apple", "pear", "grapes")
+#print(type(tuple))
+#fruitList=["apple", "pear", "grapes"]
+#print(type(fruitList))
+#complexNumber = '10i'
+#print(type(complexNumber))
+#floatNumber= 10.5
+#print(type(floatNumber))
+#integerNumber=10
+#print(type(integerNumber))
---
+++ day_5/string.py
@@ -0,0 +1,42 @@
+string = "hello i am shiv and i as software engineer"
+print(string[0])
+
+print(len(string))
+print("software" in string)
+
+#string_2 = "10,20,30,40,50"
+#result="60" in string_2
+
+#print(result)
+#notinresult = "angular" not in string
+txt = "the best thing"
+print("sorry" not in txt)
+
+
+# string slicing
+
+print(string[0:5])
+
+#string slicing from start
+print(string[:10])
+
+#string slicing to the End
+print(string[3:])
+  
+  #negative string slicing
+print(string[-15:-10])
+ 
+
+ #engineer positive indexing
+ 
+ #Positive Indexing
+#E n g i n e e r
+#0 1 2 3 4 5 6 7
+
+
+#negative indexing
+#E n g i n e e r
+#-8 -7 - 6 -5 -4  -3 -2 -1
+
+
+
