df150cd7e9491ecfe34c14a218a05dd0817ba153
PranaliSatape <110046287+PranaliSatape@users.noreply.github.com>
2022-08-09 19:40:04 +0530

    Add files via upload

---
+++ day_6/#identity opeerator.py
@@ -0,0 +1,10 @@
+#identity opeerator
+x = ["grapes", "banana"]
+y = ["grapes", "banana"]
+new  = x #means grapes, banana
+print(x is new) #he 2 same aahet ka
+print(x is not new) #vegle aahet ka
+
+print(x != y)
+
+
---
+++ day_6/list_datatype.py
@@ -0,0 +1,10 @@
+#collection of different or same values
+fruitList = ["grapes", "banana", "apples"]
+print(fruitList)
+print(len(fruitList))
+numList= [1, 5, 10, 9, 3]
+booleanList = [True, False, False]
+mixDatatypeList= ["shiv", 25, True, 50000000, "male"]
+print(numList)
+print(booleanList)
+print(mixDatatypeList)
